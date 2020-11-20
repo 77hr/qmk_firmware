0c362305aea31b01894376c4d79e84a4bb4ee7f6
tam <phtang14@gmail.com>
2020-11-20 15:43:10 -0600

    [Keymap] Add personal layout for KBDPAD MKII

---
+++ keyboards/kbdfans/kbdpad_mk2/keymaps/tam/keymap.c
@@ -0,0 +1,53 @@
+/* Copyright 2019 Ryota Goto
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include QMK_KEYBOARD_H
+
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  [0] = LAYOUT_ortho_6x4( /* Base */
+    KC_ESC,  KC_F1,   KC_F2,   KC_BSPC,
+		MO(1),   KC_PSLS, KC_PAST, KC_PMNS,
+		KC_7,    KC_8,    KC_9,    KC_PPLS,
+		KC_4,    KC_5,    KC_6,    KC_PPLS,
+		KC_1,    KC_2,    KC_3,    KC_PENT,
+		KC_0,    KC_0,    KC_DOT,  KC_PENT
+  ),
+
+	[1] = LAYOUT_ortho_6x4( /* Fn1 Layer */
+		RESET,   KC_LSFT, KC_TRNS, KC_TAB,
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_HOME, KC_UP,   KC_PGUP, KC_TRNS,
+		KC_LEFT, KC_TRNS, KC_RGHT, KC_TRNS,
+		KC_END,  KC_DOWN, KC_PGDN, KC_SPC,
+		KC_INS,  KC_INS,  KC_DEL,  KC_SPC
+	)
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  return true;
+}
+
+void matrix_init_user(void) {
+
+}
+
+void matrix_scan_user(void) {
+
+}
+
+void led_set_user(uint8_t usb_led) {
+
+}
---
+++ keyboards/kbdfans/kbdpad_mk2/keymaps/tam/readme.md
@@ -0,0 +1,16 @@
+## Default layer
+
+![Default layer layout](https://i.imgur.com/tTeNqnF.png)
+
+([KLE](http://www.keyboard-layout-editor.com/##@@=Esc&=F1&=F2&=Bksp%3B&@_y:0.25%3B&=Fn&=%2F%2F&=*&=-%3B&@=7&=8&=9&_h:2%3B&=+%3B&@=4&=5&=6%3B&@=1&=2&=3&_h:2%3B&=Enter%3B&@_w:2%3B&=0&=.))
+
+## Function 1 layer
+
+![Function 1 layer layout](https://i.imgur.com/8jQmI1E.png)
+
+([KLE](http://www.keyboard-layout-editor.com/##@@=Reset&=Shift&_g:true&a:7%3B&=&_g:false&a:4%3B&=Tab%3B&@_y:0.25&g:true&a:7%3B&=&=&=&=%3B&@_g:false&a:4%3B&=Home&=%E2%86%91&=Page%20Up&_g:true&a:7&h:2%3B&=%3B&@_g:false&a:4%3B&=%E2%86%90&_g:true&a:7%3B&=&_g:false&a:4%3B&=%E2%86%92%3B&@=End&=%E2%86%93&=Page%20Down&_h:2%3B&=Space%3B&@_w:2%3B&=Insert&=Delete))
+
+Compile and flash:
+```
+make kbdfans/kbdpad_mk2:tam:flash
+```
